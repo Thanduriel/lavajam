3185c2bc75688183b521a8573d83d846c6c408cf
Fin Christensen <christensen.fin@gmail.com>
2017-10-21 19:23:22 +0200

    Add smooth controls

--- src/components/controllercomponent.cpp
+++ src/components/controllercomponent.cpp
@@ -7,14 +7,14 @@ Component(actor, isActive),m_keyMapping(keyMapping){};
 
 void ControllerComponent::Process(float deltaTime)
 {
-    float translate =0;
-    float rotate=0;
-    if(Input::KeyManager::getKeyStates(m_keyMapping[0])==Input::EKeyState::PRESS) translate+=deltaTime;
-    if(Input::KeyManager::getKeyStates(m_keyMapping[2])==Input::EKeyState::PRESS) translate-=deltaTime;
+    float translate = 0;
+    float rotate = 0;
+    if(Input::KeyManager::getKeyStates(m_keyMapping[0])==Input::EKeyState::PRESS) translate-=deltaTime;
+    if(Input::KeyManager::getKeyStates(m_keyMapping[2])==Input::EKeyState::PRESS) translate+=deltaTime;
     if(Input::KeyManager::getKeyStates(m_keyMapping[1])==Input::EKeyState::PRESS) rotate-=deltaTime;
     if(Input::KeyManager::getKeyStates(m_keyMapping[3])==Input::EKeyState::PRESS) rotate+=deltaTime;
-    m_actor->AddVelocity(glm::vec2(0,translate));   
-    m_actor->AddRotation(rotate);
+    m_actor->AddVelocity(glm::vec2(0,translate / 100.0f));   
+    m_actor->AddRotation(rotate * 5);
     if(Input::KeyManager::getKeyStates(m_keyMapping[4])==Input::EKeyState::PRESS) {
         //shoot!
     }
