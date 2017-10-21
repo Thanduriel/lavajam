2ed61c5bf8fa49a55ae11354e88cde4a1412f552
Maximilian Deubel <maximilian.deubel@ovgu.de>
2017-10-21 18:08:56 +0200

    change controller behaviour

--- src/components/controllercomponent.cpp
+++ src/components/controllercomponent.cpp
@@ -7,13 +7,14 @@ Component(actor, isActive),m_keyMapping(keyMapping){};
 
 void ControllerComponent::Process(float deltaTime)
 {
-    float x=0;
-    float y=0;
-    if(Input::KeyManager::getKeyStates(m_keyMapping[0])==Input::EKeyState::PRESS) y+=1;
-    if(Input::KeyManager::getKeyStates(m_keyMapping[1])==Input::EKeyState::PRESS) x-=1;
-    if(Input::KeyManager::getKeyStates(m_keyMapping[2])==Input::EKeyState::PRESS) y-=1;
-    if(Input::KeyManager::getKeyStates(m_keyMapping[3])==Input::EKeyState::PRESS) x+=1;
-    m_actor->AddVelocity(glm::vec2(x*deltaTime,y*deltaTime));    
+    float translate =0;
+    float rotate=0;
+    if(Input::KeyManager::getKeyStates(m_keyMapping[0])==Input::EKeyState::PRESS) translate+=deltaTime;
+    if(Input::KeyManager::getKeyStates(m_keyMapping[2])==Input::EKeyState::PRESS) translate-=deltaTime;
+    if(Input::KeyManager::getKeyStates(m_keyMapping[1])==Input::EKeyState::PRESS) rotate-=deltaTime;
+    if(Input::KeyManager::getKeyStates(m_keyMapping[3])==Input::EKeyState::PRESS) rotate+=deltaTime;
+    m_actor->AddVelocity(glm::vec2(0,transform));   
+    m_actor->AddRotation(rotate);
     if(Input::KeyManager::getKeyStates(m_keyMapping[4])==Input::EKeyState::PRESS) {
         //shoot!
     }
