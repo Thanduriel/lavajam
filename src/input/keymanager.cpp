1cdb60475153036e52f6dae922a03520ab5a48dc
Maximilian Deubel <maximilian.deubel@ovgu.de>
2017-10-21 02:24:59 +0200

    Merged

--- src/input/keymanager.cpp
+++ src/input/keymanager.cpp
@@ -9,8 +9,11 @@ namespace Input {
 
     std::unordered_map<int,int> KeyManager::m_pressedKeys;
     GLFWwindow* KeyManager::m_window;   
+    KeyManager* KeyManager::m_instance;
+    int KeyManager::activeJoysticks[16];    
 
-    void KeyManager::key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
+    void KeyManager::key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+    {
         m_pressedKeys.insert_or_assign(key,action);
         #ifndef NDEBUG
             std::cout << "keycode:" <<  key << " " << action << std::endl;
@@ -20,8 +23,48 @@ namespace Input {
 	void KeyManager::Init(GLFWwindow* win)
 	{
 		m_window = win;
-		glfwSetKeyCallback(win, key_callback);
+        glfwSetKeyCallback(win, key_callback);
+        glfwSetKeyCallback(win, key_callback);
+        glfwSetCursorPosCallback(win, cursor_pos_callback);  
+        glfwSetCursorEnterCallback(win, cursor_enter_callback);
+        glfwSetMouseButtonCallback(win, mouse_button_callback); 
 	}
 
+    void KeyManager::cursor_pos_callback(GLFWwindow* window, double xpos, double ypos)
+    {
+        #ifndef NDEBUG
+            std::cout << "Mouse X:" <<  xpos << " Y: " << ypos << std::endl;
+        #endif
+    }    
+    void KeyManager::cursor_enter_callback(GLFWwindow* window, int entered)
+    {
+        #ifndef NDEBUG
+        
+            if (entered)
+            {
+                std::cout << "Hey hey! Welcome cursor!" << std::endl;
+            }
+            else
+            {
+                std::cout << "Oh noes! Lonely yet again!" << std::endl;
+            }
+        #endif
+    }
+    void KeyManager::mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+    {
+        #ifndef NDEBUG
+            std::cout << "Mouseclick, Button: " << button << " Action: " << action << std::endl;
+        #endif
+    }    
+
+
+    void KeyManager::pollEvents(){
+        for(size_t i= 0; i<16; i++){
+            activeJoysticks[i]=glfwJoystickPresent(i);            
+            if(GLFW_TRUE==activeJoysticks[i] ){
+                std::cout << "Found Joystick Nr. " << (i+1) << std::endl;
+            }
+        }
+    }
 
 }
--- src/input/keymanager.hpp
+++ src/input/keymanager.hpp
@@ -10,8 +10,14 @@ class KeyManager
 {
 private:
     static GLFWwindow* m_window;   
+    static KeyManager* m_instance;
+    
     static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);   
+    static void cursor_pos_callback(GLFWwindow* window, double xpos, double ypos);
+    static void cursor_enter_callback(GLFWwindow* window, int entered);    
+    static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
     static std::unordered_map<int,int> m_pressedKeys;
+    static int activeJoysticks[];
 public:
 	static void Init(GLFWwindow* win);
 
